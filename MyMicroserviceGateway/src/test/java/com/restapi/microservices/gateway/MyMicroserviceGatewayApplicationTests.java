75bb1616319a5fb84ef52e465092c3692671fc2f
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-05-05 17:26:43 +0530

    gateway+discovery microservice

---
+++ MyMicroserviceGateway/src/test/java/com/restapi/microservices/gateway/MyMicroserviceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMicroserviceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
